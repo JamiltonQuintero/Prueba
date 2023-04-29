5a87eb851f57a4f1d519376a503ebe85b6230c22
JamiltonQuintero <transportador1118@gmail.com>
2023-04-29 16:51:41 -0500

    test 5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@ComponentScan({ "com.example.demo" })
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class DemoApplication {
 
 	public static void main(String[] args) {
